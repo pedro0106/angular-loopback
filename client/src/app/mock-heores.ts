95d4f8e09b20a1fca830d6066cfa2114d1948328
Pedro Henrique de Souza Silva <2016118050327@ifpi.int>
2018-11-07 00:57:05 -0200

    Aula do dia 20/11/2018

---
+++ client/src/app/mock-heores.ts
@@ -0,0 +1,16 @@
+import { Hero } from "./hero";
+
+
+export const HEROES : Hero[]= [
+  
+    {id : 11, name : "Batman"},
+    {id : 12, name : "Homem Aranha"},
+    {id : 13, name:"Mulher Maravilha"}
+];
+
+
+
+
+
+
+
